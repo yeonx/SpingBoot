6956ae7231cced59858d32bfb0f5490b89fd399d
yeonx <71878202+yeonx@users.noreply.github.com>
2022-03-08 23:35:09 +0900

    Update HelloController.java

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -2,9 +2,9 @@ package com.jojoldu.book.springboot.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello") // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어줌
     public String hello(){
         return "hello";
     }
