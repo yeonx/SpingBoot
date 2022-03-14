e78cf7663960d02f476589a1f478725c347fedb5
yeonx <71878202+yeonx@users.noreply.github.com>
2022-03-15 08:10:43 +0900

    Update Application.java

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,9 +1,12 @@
 package com.jojoldu.book.springboot;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
-public class Application {
+public class Application { //앞으로 만들 프로젝트의 메인 클래스
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
     }
