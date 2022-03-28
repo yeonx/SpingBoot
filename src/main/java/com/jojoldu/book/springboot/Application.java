cc2736e014630d1c4f8ecbcad5ab3fd40fa58abd
yeonx <71878202+yeonx@users.noreply.github.com>
2022-03-28 11:42:35 +0900

    Update Application.java

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+// 삭제 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application { //앞으로 만들 프로젝트의 메인 클래스
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
     }
 }
-
-// Application 클래스는 앞으로 만들 프로젝트의 메인 클래스가 됨
-// @SpringBootApplication으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
-// 특히나 @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야만 함
